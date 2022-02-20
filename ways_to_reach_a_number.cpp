183da16dcab05c35d0ec46c2d6cff9cc4f99ec8a
HarshallSalunke <harshallsalunke46@gmail.com>
2022-02-20 12:13:18 +0530

    commit

--- ways_to_reach_a_number.cpp
+++ ways_to_reach_a_number.cpp
@@ -1,13 +1,32 @@
+// t
+// n k(number)
+// a[n]
+
 #include<bits/stdc++.h>
 
 using namespace std;
 
 int main() {
-    cout << "ways to reach";
-    cout << "a number using many ways";
-    int a ;
-    cin >> a;
-    cout << a;
+    int t ;
+    cin >> t;
+    while(t--) {
+        int n,k;
+        vector<int> a;
+        cin >> n >> k;
+        for(int i=0;i<n;i++) {
+            int tmp;
+            cin >> tmp;
+            a.push_back(tmp);
+        }
+        int b[k+1] = {0};
+        b[0] = 1;
+        for(int i=0;i<n;i++) {
+            for(int j=a[i];j<=k;j++) {
+                b[j]+=b[j-a[i]];
+            }
+        }
+        cout << b[k] << endl;
+    }
     return 0;
 
 
