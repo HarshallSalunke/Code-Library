f38069104afc29e79970f2640b799eaa63dc37fb
HarshallSalunke <59998998+HarshallSalunke@users.noreply.github.com>
2022-04-12 00:22:41 +0530

    Create Number_of_1_bits.cpp

---
+++ Number_of_1_bits.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    int setBits(int N) {
+        // Write Your Code here
+        int cnt=0;
+        while(N) {
+            if(N%2 != 0) cnt++;
+            N = N>>1;
+        }
+        return cnt;
+    }
+};
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+
+        Solution ob;
+        int cnt = ob.setBits(N);
+        cout << cnt << endl;
+    }
+    return 0;
+}
