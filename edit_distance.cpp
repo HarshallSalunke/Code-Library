6289eed7cebef66c686241d5ec524e5fffec7e3e
HarshallSalunke <59998998+HarshallSalunke@users.noreply.github.com>
2022-04-12 11:57:51 +0530

    Create edit_distance.cpp

---
+++ edit_distance.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int dist(string s1,string s2){
+    int m = s1.length();
+    int n = s2.length();
+    int dp[m+1][n+1];
+    for(int i=0;i<=m;i++){
+        for(int j=0;j<=n;j++){
+            if(i==0) dp[i][j] = j;
+            else if(j==0) dp[i][j] = i;
+            else if(s1[i-1] == s2[j-1]){
+                dp[i][j] = dp[i-1][j-1];
+            }
+            else{
+                dp[i][j] = 1 + min(dp[i-1][j-1],min(dp[i-1][j],dp[i][j-1]));
+            }
+        }
+    }
+    for(int i=0;i<=m;i++){
+        for(int j=0;j<=n;j++){
+            cout<<dp[i][j]<<" ";
+        }
+        cout<<"\n";
+    }
+    return dp[m][n];
+}
+int main(){
+    string str1; 
+    string str2 ;
+    cin >> str1 >> str2;
+    cout<<dist(str1,str2)<<"\n";
+}
